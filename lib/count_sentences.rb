47c2715d0181c4ad2cbdf0fb41f7ea0f0d9e08a5
tmharkins <tmharkins@gmail.com>
2016-05-06 13:18:41 +0000

    nitrous-backup at Fri May  6 13:18:41 UTC 2016

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
-require 'pry'
-
+require 'pry' 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
   def count_sentences
-
+  	sentence_array = self.split(/[.|?|!]/)
+  	sentence_array.delete_if {|x| x == ""} 
+  	sentence_count = sentence_array.length
+  	sentence_count
   end
 end
